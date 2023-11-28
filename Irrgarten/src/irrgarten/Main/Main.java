5eaf6db295a18271f5b9604fd35bc490e7e9ab2a
Florin Emanuel Todor <florinemanueltodor@gmail.com>
2023-11-28 18:57:36 +0100

    PRACTICA 5

--- Irrgarten/src/irrgarten/Main/Main.java
+++ Irrgarten/src/irrgarten/Main/Main.java
@@ -1,21 +1,22 @@
 package irrgarten.Main;
 
 import irrgarten.UI.TextUI;
+import irrgarten.UI.UI;
 import irrgarten.controller.Controller;
 import irrgarten.Game;
-import irrgarten.ShieldCardDeck;
+import irrgarten.UI.GameUI;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
 
-        int nplayer = 1;
+        int nplayer = 2;
+
         // Crear instancias del juego, la vista y el controlador
         Game game = new Game(nplayer);
-        TextUI textUI = new TextUI();
-        Controller controller = new Controller(game, textUI);
+        UI ui = GameUI.getInstance();  // Puedes cambiar a otra implementación de UI en el futuro
+        Controller controller = new Controller(game, ui);
 
         // Iniciar el juego a través del controlador
         controller.play();
-
     }
 }
